86ba264814bd87f0c7bd5c14a0369ede620e1728
albertoc <albertocaballero6@hotmail.es>
2019-02-06 17:47:01 +0100

    Python exercises

---
+++ session-2/sum.py
@@ -0,0 +1,7 @@
+#sum of the first 100 integer numbers (1+2+3...+100)
+
+
+sum = 0
+for i in range(100):
+    sum = sum + i + 1
+print("The result of the sum of the first 100 integer numbers : ", sum)
