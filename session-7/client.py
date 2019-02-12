68d26e4fdb0aa575a7404adc704598667945785c
albertoc <albertocaballero6@hotmail.es>
2019-02-12 16:47:35 +0100

    Python exercises

--- session-7/client.py
+++ session-7/client.py
@@ -6,3 +6,21 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("Socket created")
+
+PORT = 8080
+IP = "212.128.253.64"
+
+# Connect to the server
+s.connect((IP, PORT))
+
+s.send(str.encode("HELLO FROM MY CLIENT"))
+
+msg = s.recv(2048).decode("utf-8")
+print("MESSAGE FORM THE SERVER: ")
+print(msg)
+
+s.close()
+
+
+print("The end")
+
