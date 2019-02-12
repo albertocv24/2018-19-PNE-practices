979c32a95746de2160f823c881d4f6a5fc4a69dc
albertoc <albertocaballero6@hotmail.es>
2019-02-12 16:14:16 +0100

    Python exercises

---
+++ session-7/file_read.py
@@ -0,0 +1,15 @@
+# Example of reading a file located
+# in our local filesystem
+
+NAME = "mynotes.txt"
+
+# Open the file, my file is an object
+myfile = open(NAME, 'r')
+
+print("File opened : {}".format(myfile.name))
+
+contents = myfile.read()
+
+print("The file contents are: {}".format(contents))
+
+myfile.close()
