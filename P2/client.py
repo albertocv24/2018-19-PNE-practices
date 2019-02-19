e9e9ab9b31f103d959523ed970e3e96634524ee3
albertoc <albertocaballero6@hotmail.es>
2019-02-19 16:14:28 +0100

    Python exercises

---
+++ P2/client.py
@@ -0,0 +1,33 @@
+# Programming our fist client
+
+import socket
+from seqp2 import Seq
+
+
+# create a socket for communicating with the server)
+
+
+print('socket created')
+
+PORT= 8080
+IP= '212.128.253.91'
+
+
+
+while True:
+    sequence= input('Enter the sequence that you want to analyze: ')
+    s1 = Seq(sequence)
+    compl=(s1.complement())
+    rev=(s1.reverse())
+    data= '\nThe complementary sequence is: {0}\nThe reverse sequence is {1}'.format(compl, rev)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, PORT))
+    s.send(str.encode(data))
+    s.close()
+
+msg= s.recv(2048).decode('utf-8')
+print('message from the server: ')
+print(msg)
+
+print('the end')
