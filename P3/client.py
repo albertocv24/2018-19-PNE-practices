57ee5828f4ad7eb395c01641f206ab863d8312d2
albertoc <albertocaballero6@hotmail.es>
2019-03-26 15:13:01 +0100

    Python exercises

---
+++ P3/client.py
@@ -0,0 +1,17 @@
+import socket
+
+port = 8046
+IP = "212.128.253.114"
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP, port))
+msg = """
+"""
+
+s.send(msg.encode())
+
+info = s.recv(2048).decode('utf-8')
+print(info)
+
+s.close()
